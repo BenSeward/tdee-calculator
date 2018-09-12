4c79d957f6e45849e4d6ab27136f257e746480be
Ben Seward <ben.seward@ITS0647.local>
2018-09-12 13:43:06 +0100

    initial commit

---
+++ src/components/Calculation.js
@@ -0,0 +1,13 @@
+// Formula can be found here http://www.superskinnyme.com/calculate-tdee.html
+
+class Calculation {
+    calculateBMR(gender, age, height, weight) {
+        if (gender === 'male') {
+
+        } else {
+
+        }
+    }
+}
+
+export default Calculation;
---
+++ src/components/Form.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import Calculation from './Calculation';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      age: '',
+      gender: '',
+      height:'',
+      weight: '',
+      value: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event) {
+    switch(event.target.name) {
+      case 'name':
+        this.setState({
+          name: event.target.value
+        });
+        break;
+      case 'age':
+        this.setState({
+          age: event.target.value
+        });
+        break;
+      case 'height':
+        this.setState({
+          height: event.target.value
+        });
+        break;
+      case 'weight':
+        this.setState({
+          weight: event.target.value
+        });
+        break;
+      case 'gender':
+        this.setState({
+          gender: event.target.value
+        });
+        break;
+    }
+
+    console.log(event.target)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input 
+            type="radio" 
+            value={this.state.gender} 
+            onChange={this.handleChange} 
+            name="gender" 
+            value="male"
+          />
+          <label for="gender">Male</label>
+          <input 
+            type="radio" 
+            value={this.state.gender} 
+            onChange={this.handleChange} 
+            name="gender" 
+            value="female" 
+          />
+          <label for="gender">Female</label>
+          <input 
+            type="text" 
+            value={this.state.name} 
+            onChange={this.handleChange} 
+            name="name"
+            placeholder="Name"
+          />
+          <input 
+            type="text" 
+            value={this.state.age} 
+            onChange={this.handleChange} 
+            name="age"
+            placeholder="Age" />
+          <input 
+            type="text" 
+            value={this.state.height} 
+            onChange={this.handleChange} 
+            name="height"
+            placeholder="Height (cms)" />
+          <input 
+            type="text" 
+            value={this.state.weight} 
+            onChange={this.handleChange} 
+            name="weight"
+            placeholder="Weight (kgs)" />
+          <input type="submit" 
+          value="Calculate" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
